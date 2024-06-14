229be1de00fb21a7339185b2fccbac385865a4eb
Danny Berger <danny.berger@fgsglobal.com>
2024-06-14 17:03:55 -0400

    chore: Add server-only-context package and instantiate

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -2,10 +2,13 @@ import { notFound } from "next/navigation";
 import generateMarkup from "../markup";
 import pages from "../data";
 import renderingEngine from "@/util/rendering-engine";
+import { getPageContext } from "@/util/server-context";
 
 export type PageProps = {
   params: { id: string; slug: string[] };
 };
+
+const pageContext = getPageContext();
 
 // Force cache-on-demand!
 export function generateStaticParams() {
